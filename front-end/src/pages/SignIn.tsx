d0d787ce4a6628b373538346e4fa3ab7ee8aeeb1
Thisara Chathuranga <129034261+thisaraChathuranga000@users.noreply.github.com>
2024-04-28 22:53:05 +0530

    new

--- front-end/src/pages/SignIn.tsx
+++ front-end/src/pages/SignIn.tsx
@@ -1,6 +1,37 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+interface UserData {
+  id: number;
+  userName: string;
+  password: string;
+  svcNo: string;
+  name: string;
+  intake: string;
+  platoon: string;
+}
 
 function SignIn() {
+  const [postData, setPostData] = useState<UserData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/users");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data: UserData[] = await response.json();
+        setPostData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // You might want to set an empty array or handle the error differently
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
         <h2>Sign In</h2>
@@ -24,6 +55,10 @@ function SignIn() {
         <br/>
 
         <button>Sign In</button>
+
+        
+
+         
 
     </div>
   )
