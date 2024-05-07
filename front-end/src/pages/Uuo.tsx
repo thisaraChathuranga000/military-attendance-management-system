c89b6dec472a5f336601c8393bcfac1c1acc97ac
Thisara Chathuranga <139837131+thisaraChathuranga00@users.noreply.github.com>
2024-05-07 14:16:52 +0530

    new

--- front-end/src/pages/Uuo.tsx
+++ front-end/src/pages/Uuo.tsx
@@ -1,12 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { RootState } from '../redux/store';
 import { setSelectedDate } from '../redux/slice/dateSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { Box, Grid } from '@mui/material';
 import ParticipantTable from '../component/ParticipantTable';
-import { listProps } from '../component/ParticipantTable';
-import AbsentParticipantTable, { AbsentAttendedParticipantListProps, AbsentNotAttendedParticipantListProps } from '../component/AbsentParticipantTable';
+import AbsentParticipantTable from '../component/AbsentParticipantTable';
 import { logout } from '../redux/slice/authSlice';
+import { 
+  useGetAttendanceStatsQuery, 
+  useGetAttendedUserDataQuery, 
+  useGetNotAttendedUserDataQuery, 
+  useGetNotAttendedUserDataReasonQuery,
+  useGetAbsentAttendedUserDataQuery,
+  useGetAbsentNotAttendedUserDataQuery,
+  useGetAbsentNotAttendedUserDataReasonQuery
+ } from '../redux/services/Attendance';
 
 function Uuo() {
     const selectedDate = useSelector((state: RootState) => state.date.selectedDate);
@@ -15,140 +23,39 @@ function Uuo() {
     const [showOnParade, setShowOnParade] = useState<boolean>(false);
     const [openNotOnParadeList, setOpenNotOnParadeList] = useState<boolean>(false);
     const [showNotOnParade, setShowNotOnParade] = useState<boolean>(false);
-    const [attendedUsers, setAttendedUsers] = useState<listProps[]>([]);
-    const [notAttendedUsers, setNotAttendedUsers] = useState<listProps[]>([]);
-    const [notAttendedReasons, setNotAttendedReasons] = useState<any[]>([]);
     const [showAbsentUsers, setShowAbsentUsers] = useState<boolean>(false);
     const [openAbsentUsersList, setOpenAbsentUsersList] = useState<boolean>(false);
-    const [absentAttendedUsers, setAbsentAttendedUsers] = useState<AbsentAttendedParticipantListProps[]>([]);
-    const [absentNotAttendedUsers, setAbsentNotAttendedUsers] = useState<AbsentNotAttendedParticipantListProps[]>([]);
-    const [absentNotAttendedReasons, setAbsentNotAttendedReasons] = useState<any[]>([]);
-    
 
+    const { data:attendance, error:attendanceError, isLoading:attendanceIsLoading } = useGetAttendanceStatsQuery(selectedDate);
+    const { data:attendedUsersData, error:attendedUsersError, isLoading:attendedUsersIsLoading } = useGetAttendedUserDataQuery(selectedDate);
+    const { data:notAttendedUsersData, error:notAttendedUsersError, isLoading:notAttendedUsersIsLoading } = useGetNotAttendedUserDataQuery(selectedDate);
+    const { data:notAttendedUsersDataReason, error:notAttendedUsersReasonError, isLoading:notAttendedUsersReasonIsLoading } = useGetNotAttendedUserDataReasonQuery(selectedDate);
+    const { data:absentAttendedUserData, error:absentAttendedUserDataError, isLoading:absentAttendedUserDataIsLoading } = useGetAbsentAttendedUserDataQuery(selectedDate);
+    const { data:absentNotAttendedUserData, error:absentNotAttendedUserDataError, isLoading:absentNotAttendedUserDataIsLoading } = useGetAbsentNotAttendedUserDataQuery(selectedDate);
+    const { data:absentNotAttendedUserDataReason, error:absentNotAttendedUserDataReasonError, isLoading:absentNotAttendedUserDataReasonIsLoading } = useGetAbsentNotAttendedUserDataReasonQuery(selectedDate);
+    
+    const handleCloseOnParadeList = () => {setOpenOnParadeList(false)};
+    const handleCloseNotOnParadeList = () => {setOpenNotOnParadeList(false)};
+    const handleClickNotOpenAbsentList = () => {setOpenAbsentUsersList(false)};
+    
     const handleClickOpenOnParadeList = () => {
       setOpenOnParadeList(true);
       setShowOnParade(true);
     };
-    const handleCloseOnParadeList = () => {setOpenOnParadeList(false)};
-
+    
     const handleClickOpenNotOnParadeList = () => {
       setOpenNotOnParadeList(true);
       setShowNotOnParade(true);
     }
-    const handleCloseNotOnParadeList = () => {setOpenNotOnParadeList(false)};
-
+    
     const handleClickOpenAbsentList = () => {
       setOpenAbsentUsersList(true);
       setShowAbsentUsers(true);
     }
 
-    const handleClickNotOpenAbsentList = () => {setOpenAbsentUsersList(false)};
-
-    const [attendanceStats, setAttendanceStats] = useState({
-      onPerad: 0,
-      notOnPerad: 0,
-      absent: 0,
-      total: 0
-    });
-
     const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         dispatch(setSelectedDate(event.target.value));
-        fetchData(event.target.value);
     };
-
-    const fetchData = async (date: string) => {
-        try {
-            const response = await fetch(`http://localhost:5000/attendance/stats/${date}`);
-            if (response.ok) {
-                const data = await response.json();
-                setAttendanceStats(data);
-            } else {
-                console.error('Failed to fetch attendance stats');
-            }
-        } catch (error) {
-            console.error('Error fetching attendance stats:', error);
-        }
-    };
-
-    const fetchAttendedUserData = async (date:string) => {
-      try {
-          const response = await fetch(`http://localhost:5000/attendance/attended-users/${date}`);
-          if (response.ok) {
-              const data = await response.json();
-              setAttendedUsers(data);
-          } else {
-              console.error('Failed to fetch attended users data');
-          }
-      } catch (error) {
-          console.error('Error fetching attended users data:', error);
-      }
-    };
-
-    const fetchNotAttendedUserData = async (date:string) => {
-      try {
-          const usersResponse = await fetch(`http://localhost:5000/attendance/not-attended-users/${date}`);
-          if (usersResponse.ok) {
-              const usersData = await usersResponse.json();
-              setNotAttendedUsers(usersData);
-          } else {
-              console.error('Failed to fetch not attended users data');
-          }
-
-          const reasonsResponse = await fetch(`http://localhost:5000/attendance/not-attended-users/reason/${date}`);
-          if (reasonsResponse.ok) {
-              const reasonsData = await reasonsResponse.json();
-              setNotAttendedReasons(reasonsData);
-          } else {
-              console.error('Failed to fetch not attended reasons data');
-          }
-      } catch (error) {
-          console.error('Error fetching data:', error);
-      }
-    };
-
-    const fetchAbsentAttendedUserData = async (date:string) => {
-      try {
-          const response = await fetch(`http://localhost:5000/attendance/absent/attended-users/${date}`);
-          if (response.ok) {
-              const data = await response.json();
-              setAbsentAttendedUsers(data);
-          } else {
-              console.error('Failed to fetch attended users data');
-          }
-      } catch (error) {
-          console.error('Error fetching attended users data:', error);
-      }
-    };
-
-    const fetchAbsentNotAttendedUserData = async (date:string) => {
-      try {
-          const usersResponse = await fetch(`http://localhost:5000/attendance/absent/not-attended-users/${date}`);
-          if (usersResponse.ok) {
-              const usersData = await usersResponse.json();
-              setAbsentNotAttendedUsers(usersData);
-          } else {
-              console.error('Failed to fetch not attended users data');
-          }
-
-          const reasonsResponse = await fetch(`http://localhost:5000/attendance/absent/not-attended-users/reason/${date}`);
-          if (reasonsResponse.ok) {
-              const reasonsData = await reasonsResponse.json();
-              setAbsentNotAttendedReasons(reasonsData);
-          } else {
-              console.error('Failed to fetch not attended reasons data');
-          }
-      } catch (error) {
-          console.error('Error fetching data:', error);
-      }
-    };
-
-    useEffect(() => {
-        fetchData(selectedDate);
-        fetchAttendedUserData(selectedDate);
-        fetchNotAttendedUserData(selectedDate);
-        fetchAbsentAttendedUserData(selectedDate);
-        fetchAbsentNotAttendedUserData(selectedDate);
-    }, [selectedDate]);
 
     const handleLogOut = () => {
       window.location.replace('http://localhost:3000/sign-in')
@@ -193,69 +100,93 @@ function Uuo() {
              
 
             <input type="date" id="datePicker" value={selectedDate} onChange={handleDateChange} style={{ padding:5, border:"1px solid #C68D4D", borderRadius:"5px", marginBottom:30}} />
+ 
+            {attendanceError ? (
+              <>Oh no, there was an error</>
+            ) : attendanceIsLoading ? (
+              <>Loading...</>
+            ) : attendance ? (
+              <Grid container spacing={0}>
+                <Grid item lg={3} xs={6} md={8} sx={{backgroundColor:"#DAC0A3"}}>
+                  <div style={{ display:"flex", flexDirection:"row", alignItems:"center"}}>
+                    <p style={{paddingLeft:"25px", fontWeight:"500"}}>Total <br/>Participants</p>  
+                    <h1 style={{paddingLeft:"35px", color:"#C68D4D", fontSize:"46px"}}>{attendance.total}</h1>               
+                  </div>
+                </Grid>
 
-            <Grid container spacing={0}>
-              <Grid item lg={3} xs={6} md={8} sx={{backgroundColor:"#DAC0A3"}}>
-                <div style={{ display:"flex", flexDirection:"row", alignItems:"center"}}>
-                  <p style={{paddingLeft:"25px", fontWeight:"500"}}>Total <br/>Participants</p>  
-                  <h1 style={{paddingLeft:"35px", color:"#C68D4D", fontSize:"46px"}}>{attendanceStats.total}</h1>               
-                </div>
+                <Grid item lg={3} xs={6} md={4} sx={{backgroundColor:"#EADBC8"}} onClick={handleClickOpenOnParadeList}>
+                  <div style={{ display:"flex", flexDirection:"row", alignItems:"center"}}>
+                    <p style={{paddingLeft:"25px", fontWeight:"500"}}>On Parade <br/>Participants</p>  
+                    <h1 style={{paddingLeft:"35px", color:"#C68D4D", fontSize:"46px"}}>{attendance.onPerad}</h1>               
+                  </div>
+                </Grid>
+
+                <Grid item lg={3} xs={6} md={4} sx={{backgroundColor:"#DAC0A3"}} onClick={handleClickOpenNotOnParadeList}>
+                  <div style={{ display:"flex", flexDirection:"row", alignItems:"center"}}>
+                    <p style={{paddingLeft:"25px", fontWeight:"500"}}>Not on Parade<br/>Participants</p>  
+                    <h1 style={{paddingLeft:"35px", color:"#C68D4D", fontSize:"46px"}}>{attendance.notOnPerad}</h1>               
+                  </div>
+                </Grid>
+
+                <Grid item lg={3} xs={6} md={8} sx={{backgroundColor:"#EADBC8"}} onClick={handleClickOpenAbsentList}>
+                  <div style={{ display:"flex", flexDirection:"row", alignItems:"center"}}>
+                    <p style={{paddingLeft:"25px", fontWeight:"500"}}>Non Responded <br/>Participants</p>  
+                    <h1 style={{paddingLeft:"35px", color:"#C68D4D", fontSize:"46px"}}>{attendance.absent}</h1>               
+                  </div>
+                </Grid>
               </Grid>
-              <Grid item lg={3} xs={6} md={4} sx={{backgroundColor:"#EADBC8"}} onClick={handleClickOpenOnParadeList}>
-                <div style={{ display:"flex", flexDirection:"row", alignItems:"center"}}>
-                  <p style={{paddingLeft:"25px", fontWeight:"500"}}>On Parade <br/>Participants</p>  
-                  <h1 style={{paddingLeft:"35px", color:"#C68D4D", fontSize:"46px"}}>{attendanceStats.onPerad}</h1>               
-                </div>
-              </Grid>
-              <Grid item lg={3} xs={6} md={4} sx={{backgroundColor:"#DAC0A3"}} onClick={handleClickOpenNotOnParadeList}>
-                <div style={{ display:"flex", flexDirection:"row", alignItems:"center"}}>
-                  <p style={{paddingLeft:"25px", fontWeight:"500"}}>Not on Parade<br/>Participants</p>  
-                  <h1 style={{paddingLeft:"35px", color:"#C68D4D", fontSize:"46px"}}>{attendanceStats.notOnPerad}</h1>               
-                </div>
-              </Grid>
-              <Grid item lg={3} xs={6} md={8} sx={{backgroundColor:"#EADBC8"}} onClick={handleClickOpenAbsentList}>
-                <div style={{ display:"flex", flexDirection:"row", alignItems:"center"}}>
-                  <p style={{paddingLeft:"25px", fontWeight:"500"}}>Non Responded <br/>Participants</p>  
-                  <h1 style={{paddingLeft:"35px", color:"#C68D4D", fontSize:"46px"}}>{attendanceStats.absent}</h1>               
-                </div>
-              </Grid>
-            </Grid>
+            ) : null} 
           </Box>
 
-          {showOnParade &&(
+        {showOnParade && attendedUsersError ? (
+            <p>error</p>
+          ): attendedUsersIsLoading ? (
+            <p>Loading...</p>
+          ): attendedUsersData ? (
             <ParticipantTable 
               open={openOnParadeList} 
               title={'On Parade Participation'} 
               handleClose={handleCloseOnParadeList}
-              list={attendedUsers}
-              />
-          )}
+              list={attendedUsersData}
+            />
+          ) : null
+        }
 
-          {showNotOnParade &&(
+        {showNotOnParade && notAttendedUsersError && notAttendedUsersReasonError ? (
+            <p>error</p>
+          ) : notAttendedUsersIsLoading && notAttendedUsersReasonIsLoading ? (
+            <p>Loading...</p>
+          ) : notAttendedUsersData && notAttendedUsersDataReason ? (
             <ParticipantTable 
               open={openNotOnParadeList} 
               title={'Not on Parade Participation'} 
               handleClose={handleCloseNotOnParadeList} 
               showReason={true}
-              list={notAttendedUsers.map((user, index) => ({
+              list={notAttendedUsersData.map((user, index) => ({
                 ...user,
-                reason: notAttendedReasons[index]?.reason
+                reason: notAttendedUsersDataReason[index]?.reason
               }))}
             />
-          )}
+          ) : null
+        }
 
-          {showAbsentUsers && (
+        {showAbsentUsers && absentAttendedUserDataError && absentNotAttendedUserDataError && absentNotAttendedUserDataReasonError ? (
+            <p>error</p>
+          ) : absentAttendedUserDataIsLoading && absentNotAttendedUserDataIsLoading && absentNotAttendedUserDataReasonIsLoading ? (
+            <p>Loading...</p>
+          ) : absentAttendedUserData && absentNotAttendedUserData && absentNotAttendedUserDataReason ? (
             <AbsentParticipantTable 
               open={openAbsentUsersList} 
               handleClose={handleClickNotOpenAbsentList} 
-              attendedList={absentAttendedUsers}
-              notAttendedList={absentNotAttendedUsers.map((user, index) => ({
+              attendedList={absentAttendedUserData}
+              notAttendedList={absentNotAttendedUserData.map((user, index) => ({
                 ...user,
-                reason: absentNotAttendedReasons[index]?.reason
+                reason: absentNotAttendedUserDataReason[index]?.reason
               }))}
               showReason={true}
-              />
-          )}
+            />
+          ) : null
+        }
         </div>
     );
 }
--- front-end/src/redux/store.ts
+++ front-end/src/redux/store.ts
@@ -5,19 +5,22 @@ import userReducer from "./slice/userSlice"
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import { persistReducer, persistStore } from "redux-persist";
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { attendanceApi } from "./services/Attendance";
 
 const persistConfig = {
     key: "root",
     version: 1,
     storage,
     stateReconcile: autoMergeLevel2,
-    blacklist:['date']
+    blacklist:['date', 'attendanceApi']
 }
  
 const rootReducer = combineReducers({
     date : dateReducer,
     auth : authReducer,
-    user : userReducer
+    user : userReducer,
+    [attendanceApi.reducerPath]: attendanceApi.reducer,
 })
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
@@ -25,12 +28,13 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 export const createStore = (options?: ConfigureStoreOptions['preloadedState'] | undefined,) =>  configureStore({
     reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(),...options,
+    getDefaultMiddleware().concat(attendanceApi.middleware),...options,
 })
 
-export const store = createStore()
-export type RootState = ReturnType<typeof store.getState>
+export const store = createStore();
+setupListeners(store.dispatch);
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
 const  persistor = persistStore(store); 
-export { persistor }
+export { persistor };
